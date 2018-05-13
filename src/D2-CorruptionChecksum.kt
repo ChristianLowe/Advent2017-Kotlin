8e8faf1c66c6b6198f8792a0b9f8e49172282e8b
Christian Lowe <christianlowe.cl@gmail.com>
2018-05-13 14:35:14 -0700

    Day 2 - Part 2

--- src/D2-CorruptionChecksum.kt
+++ src/D2-CorruptionChecksum.kt
@@ -1,11 +1,36 @@
 
 fun main(args: Array<String>) {
+    part1()
+    part2()
+}
+
+private fun part1() {
     var checksum = 0
 
     val lines = inputD2.split('\n')
     for (line in lines) {
         val numbers = line.split('\t').map { it.toInt() }
         checksum += numbers.max()!! - numbers.min()!!
+    }
+
+    println(checksum)
+}
+
+private fun part2() {
+    var checksum = 0
+
+    val lines = inputD2.split('\n')
+    for (line in lines) {
+        val numbers = line.split('\t').map { it.toInt() }.sorted()
+
+        for (i in 0 until numbers.size) {
+            val numerator = numbers.slice(i+1 until numbers.size).find { it % numbers[i] == 0 }
+
+            if (numerator != null) {
+                checksum += numerator / numbers[i]
+                break
+            }
+        }
     }
 
     println(checksum)
