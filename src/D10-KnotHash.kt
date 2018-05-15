484fe0937168127376e11afaa186974b1c4c52a9
Christian Lowe <christianlowe.cl@gmail.com>
2018-05-14 19:50:48 -0700

    Day 10 - Part 2

--- src/D10-KnotHash.kt
+++ src/D10-KnotHash.kt
@@ -1,6 +1,7 @@
 
 fun main(args: Array<String>) {
     part1()
+    part2()
 }
 
 private fun part1() {
@@ -20,4 +21,34 @@ private fun part1() {
     }
 
     println(wheel[0] * wheel[1])
+}
+
+private fun part2() {
+    val wheel = (0..255).toMutableList()
+    var currentPosition = 0
+    var skipSize = 0
+
+    val inputLengths = inputD10.toList().map { it.toInt() }
+    val lengths = inputLengths.plus(listOf(17, 31, 73, 47, 23))
+
+    for (round in (1..64)) {
+        for (length in lengths) {
+            val indices = (currentPosition until currentPosition + length).map { it % wheel.size }
+            val slice = wheel.slice(indices).reversed()
+
+            for (i in 0 until indices.size) {
+                wheel[indices[i]] = slice[i]
+            }
+
+            currentPosition = (currentPosition + length + skipSize) % wheel.size
+            skipSize++
+        }
+    }
+
+    val denseHash = (0 until 16).map {
+        wheel.slice(it * 16 until (it+1) * 16).reduce { a, b -> a xor b }.toByte()
+    }
+
+    denseHash.forEach { print(String.format("%02x", it)) }
+    println()
 }
