9128275e4be4a43c91b66c9aa0eb6ecff587334c
Christian Lowe <christianlowe.cl@gmail.com>
2018-05-19 19:22:07 -0700

    Day 20 - Part 2

--- src/D20-ParticleSwarm.kt
+++ src/D20-ParticleSwarm.kt
@@ -1,15 +1,27 @@
 import kotlin.math.abs
 
-typealias Vector = Triple<Int, Int, Int>
+typealias Vector = Triple<Long, Long, Long>
 data class Particle(val position: Vector, val velocity: Vector, val acceleration: Vector)
 
 val zeroVector = Vector(0, 0, 0)
 
-fun Vector.manhattanFrom(from: Vector): Int =
+operator fun Vector.get(index: Int): Long {
+    return when (index) {
+        0 -> first
+        1 -> second
+        2 -> third
+        else -> throw IndexOutOfBoundsException()
+    }
+}
+
+fun Vector.manhattanFrom(from: Vector): Long =
         abs(this.first - from.first) + abs(this.second - from.second) + abs(this.third - from.third)
 
 fun vectorFrom(x: String, y: String, z: String): Vector =
-        Vector(x.toInt(), y.toInt(), z.toInt())
+        Vector(x.toLong(), y.toLong(), z.toLong())
+
+operator fun Vector.plus(other: Vector): Vector =
+        Vector(first + other.first, second + other.second, third + other.third)
 
 fun main(args: Array<String>) {
     part1()
@@ -40,6 +52,39 @@ private fun part1() {
     println(particleList.indexOf(closestParticle))
 }
 
-private fun part2() {
+inline fun <T, K> Iterable<T>.duplicateKeysBy(selector: (T) -> K): List<T> {
+    val set = HashSet<K>()
+    val list = ArrayList<T>()
+    for (e in this) {
+        val key = selector(e)
+        if (!set.add(key))
+            list.add(e)
+    }
+    return list
+}
 
+private fun part2() {
+    val particleRegex
+            = """p=<(-?\d+),(-?\d+),(-?\d+)>, v=<(-?\d+),(-?\d+),(-?\d+)>, a=<(-?\d+),(-?\d+),(-?\d+)>""".toRegex()
+
+    var particleList = inputD20.split('\n')
+            .map { particleRegex.find(it)!!.groupValues }
+            .map { Particle(
+                    vectorFrom(it[1], it[2], it[3]),
+                    vectorFrom(it[4], it[5], it[6]),
+                    vectorFrom(it[7], it[8], it[9])) }
+
+    // 40-tick limit found by letting simulation run for a few minutes.
+    // Is there a way to know for sure that no more particles will collide?
+    for (tick in (1..40)) {
+        val colliding = particleList.filter {
+            it.position in particleList.duplicateKeysBy { it.position }.map { it.position }
+        }
+
+        particleList -= colliding
+        particleList = particleList.map { Particle(it.position, it.velocity + it.acceleration, it.acceleration) }
+        particleList = particleList.map { Particle(it.position + it.velocity, it.velocity, it.acceleration)}
+    }
+
+    println(particleList.size)
 }
